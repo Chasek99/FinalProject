809c407c83f5a3ee796f98c13a6eb7e1849f7af9
Chase <ck121642@students.davenport.k12.ia.us>
2017-12-04 09:47:54 -0600

    Add files via upload

---
+++ Determine.java
@@ -0,0 +1,49 @@
+package finalProject;
+
+
+	public class Determine 
+	{
+
+		public static boolean weapon(int weapon)		//Determines if user has a weapon
+		{
+			boolean hasWeapon = false;
+			if(weapon > 0)
+				hasWeapon = true;
+			return hasWeapon;
+		}
+
+		public static boolean food(int food)			//Determines if user has food
+		{
+			boolean hasFood = true;
+			if(food < 1)
+				hasFood = false;
+			return hasFood;
+		}
+
+		public static boolean vehicle(int vehicle)		//Determines if user has a vehicle
+		{
+			boolean hasVehicle = false;
+			if(vehicle > 0)
+				hasVehicle = true;
+			return hasVehicle;
+		}
+
+		public static boolean survivors(int survivors)	//Determines if user has survivors
+		{
+			boolean hasSurvivors = false;
+			if(survivors > 0)
+				hasSurvivors = true;
+			return hasSurvivors;
+		}
+
+		public static boolean inCity(int location)		//Determines user's location
+		{
+			boolean inCity = false;
+			if(location == 0)
+				inCity = true;
+			return inCity;
+		}
+		
+	}
+
+
